425bde45f1e1c0f57b29c8df470587adc6da8173
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-12-26 22:28:28 +0200

    addFirst addLast

--- src/core/OrderedCollection.java
+++ src/core/OrderedCollection.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 public interface OrderedCollection<T> extends Collection<T>{
     Optional<T> getFirst();
     Optional<T> getLast();
-    void addFirst();
-    void addLast();
+    void addFirst(T element);
+    void addLast(T element);
 }
