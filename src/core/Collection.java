fc3a53d3f57104a958d9437ef7aa5102cc45d486
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-12-31 21:16:13 +0200

    move add()

--- src/core/Collection.java
+++ src/core/Collection.java
@@ -3,7 +3,6 @@ package core;
 import java.util.Optional;
 
 public interface Collection<T> extends Iterable<T>{
-    void add(T element);
     void addAll(Collection<T> collection);
     boolean contains(T element);
     int size();
--- src/core/List.java
+++ src/core/List.java
@@ -6,5 +6,6 @@ public interface List<T> {
     void remove(T element);
     Optional<T> get(int index);
     Optional<Integer> indexOf(T element);
+    void add(T element);
 
 }
